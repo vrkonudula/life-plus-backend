407d1089661931475ff2edb8cdea3df8e74289e4
vrkonudula <vkonudula@vt.edu>
2021-02-05 15:09:53 -0500

    some minor changes

--- src/main/java/com/venkat/lifeplus/LifeplusApplication.java
+++ src/main/java/com/venkat/lifeplus/LifeplusApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LifeplusApplication {
-    //Some minor comments
+    //Some minor comments.
+    //Some more comments.
     public static void main(String[] args) {
         SpringApplication.run(LifeplusApplication.class, args);
     }
